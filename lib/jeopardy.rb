cc0f431b207b36f37ab6243a093641d2e4d04b7f
Dean Silfen <dean.silfen@gmail.com>
2015-11-27 20:09:46 -0500

    add crawler

--- lib/jeopardy.rb
+++ lib/jeopardy.rb
@@ -1,4 +1,8 @@
+# mechanize https://rubygems.org/gems/mechanize
 require "jeopardy/version"
+require "jeopardy/crawler.rb"
+require "open-uri"
+require "nokogiri"
 
 module Jeopardy
   # Your code goes here...
