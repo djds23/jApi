c11a57424543fd9cfd896f6ce76f5786138c907b
Dean Silfen <dean.silfen@gmail.com>
2015-11-28 02:08:47 -0500

    add clue to jeopardy

--- lib/jeopardy/crawler.rb
+++ lib/jeopardy/crawler.rb
@@ -6,8 +6,11 @@ class Crawler
     if page > 0
       url << "/page/#{page}"
     end
-    
+
     response = open(url)
     @resp = Nokogiri::HTML(response)
+    @resp.css("div.data.lP").map! do |node|
+      Clue.new image: node.css("img").first.src, answer: node.css("p").first.to_s
+    end
   end
 end
