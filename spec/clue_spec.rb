5c5e5f9f865e3c32dc282c39c49115322461261e
Dean Silfen <dean.silfen@gmail.com>
2015-12-26 00:49:33 -0500

    Add airdate to clue, get 100% coverage

--- spec/clue_spec.rb
+++ spec/clue_spec.rb
@@ -8,12 +8,19 @@ describe JAPI::Clue do
     expect(described_class.new(resp)).not_to be nil
   end
 
-  it 'has a clue id' do
-    expect(described_class.new(resp).clue_id).to eq 107165
+  it 'responds to expected methods' do
+    clue = described_class.new(resp)
+    expect(clue.value).to eq 200
+    expect(clue.clue_id).to eq 107165
+    expect(clue.answer).to eq "a bishop"
+    expect(clue.question).to eq "...of chess pieces"
+    expect(clue.airdate).to eq Time.parse("2010-11-03T12:00:00.000Z")
   end
 
   it 'returns a category for its category' do
-    expect(described_class.new(resp).category).to be_a JAPI::Category
-    expect(described_class.new(resp).category.title).to eq "alphabetically first"
+    clue = described_class.new(resp)
+    expect(clue.category).to be_a JAPI::Category
+    expect(clue.category.title).to eq "alphabetically first"
+    expect(clue.category_id).to eq clue.category.category_id
   end
 end
